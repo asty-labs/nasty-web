db722b70e37c042908cbc79435a23304903588df
Stanislav Tkachev <s.tkachev@gmail.com>
2013-06-16 13:09:21 +0200

    chain of responsibility introduced for FormEngine event processing

---
+++ Nasty.Mvc/SimpleErrorHandlingLink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Reflection;
+using Nasty.Core;
+using Nasty.Js;
+
+namespace Nasty.Mvc
+{
+    class SimpleErrorHandlingLink : AbstractProcessingLink
+    {
+        public override IJsExpression DoProcess()
+        {
+            try
+            {
+                return base.DoProcess();
+            }
+            catch (TargetInvocationException e)
+            {
+                return Handle(e.InnerException);
+            }
+            catch (Exception t)
+            {
+                return Handle(t);
+            }
+        }
+
+        static IJsExpression Handle(Exception e)
+        {
+            return new JsCall("alert", e.Message);
+        }
+    }
+}
---
+++ Nasty/Core/ProcessChain.cs
@@ -0,0 +1,21 @@
+﻿using Nasty.Js;
+
+namespace Nasty.Core
+{
+    public interface IProcessingLink
+    {
+        IJsExpression DoProcess();
+    }
+
+    public abstract class AbstractProcessingLink : IProcessingLink
+    {
+        private IProcessingLink _next;
+
+        public IProcessingLink Next { set { _next = value; } }
+
+        public virtual IJsExpression DoProcess()
+        {
+            return _next != null ? _next.DoProcess() : null;
+        }
+    }
+}
